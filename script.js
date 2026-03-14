d6dc8fb06cf2ef98b9f4dc18f391cb2bb556ec86
Youssef Galal El-Din <ct277@stud.uni-heidelberg.de>
2026-03-14 22:50:52 +0100

    cart count

--- script.js
+++ script.js
@@ -401,7 +401,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const scaleFactor = 16 / bodyFontSize; // shrink or grow to match 16px
 
-        const strength = 0.12; // reduce scaling intensity
+        const strength = 0.14; // reduce scaling intensity
         const containerScale = 1 + (scaleFactor - 1) * strength;
 
         document.body.classList.add("instagram-browser");
