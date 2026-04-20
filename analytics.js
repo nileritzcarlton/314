0244b0c8b96a8c7c5dd6106d3d62c2272f66cd7a
Youssef Galal El-Din <ct277@stud.uni-heidelberg.de>
2026-04-20 15:33:32 +0200

    analytics

--- analytics.js
+++ analytics.js
@@ -1,11 +1,8 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-
-// base GA init
 gtag('js', new Date());
 gtag('config', 'G-X68DQ4NPVR');
 
-// detect source (Instagram or not)
 function detectSource() {
   const ref = document.referrer || "";
 
@@ -15,11 +12,10 @@ function detectSource() {
   ) {
     return "instagram";
   }
-
+  
   return "other";
 }
 
-// send custom event to Google Analytics
 gtag('event', 'traffic_source_detected', {
   source: detectSource()
 });
