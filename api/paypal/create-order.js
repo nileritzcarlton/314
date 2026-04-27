6a70ccf5f3462f46e540992c49db7c9aaeb1fdb8
Youssef Galal El-Din <ct277@stud.uni-heidelberg.de>
2026-04-27 11:43:34 +0200

    paypal

--- api/paypal/create-order.js
+++ api/paypal/create-order.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
-
+  
   try {
     const { items, currency } = req.body;
 
